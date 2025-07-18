b1d75d7f5da51da457cbb2c0e797d17710cd5a0d
MrReptiloid <morgot1999@gmail.com>
2025-07-18 12:47:54 +0300

    refactor FileFailure to simplify method signatures for error handling

--- FreeVideoCompressor.Domain/Failures/FileFailure.cs
+++ FreeVideoCompressor.Domain/Failures/FileFailure.cs
@@ -10,27 +10,27 @@ public sealed record FileFailure(
     Exception? InnerException = null)
     : FailureBase(BaseMessage, Message, InnerException)
 {
-    public static FileFailure NotFound(string? msg, Exception? innerException = null)
+    public static FileFailure NotFound(string? msg = null, Exception? innerException = null)
     {
         return new FileFailure(FileFailureType.NotFound, FileFailureMessageKeys.NotFound, msg, innerException);
     }
 
-    public static FileFailure AccessDenied(string? msg, Exception? innerException = null)
+    public static FileFailure AccessDenied(string? msg = null, Exception? innerException = null)
     {
         return new  FileFailure(FileFailureType.AccessDenied, FileFailureMessageKeys.AccessDenied, msg, innerException);
     }
 
-    public static FileFailure InvalidPath(string? msg, Exception? innerException = null)
+    public static FileFailure InvalidPath(string? msg = null, Exception? innerException = null)
     {
         return new FileFailure(FileFailureType.InvalidPath, FileFailureMessageKeys.InvalidPath, msg, innerException);
     }
     
-    public static FileFailure AlreadyExists(string? msg, Exception? innerException = null)
+    public static FileFailure AlreadyExists(string? msg = null, Exception? innerException = null)
     {
         return new FileFailure(FileFailureType.AlreadyExists, FileFailureMessageKeys.AlreadyExists, msg, innerException);
     }
 
-    public static FileFailure Unknown(string? msg, Exception? innerException = null)
+    public static FileFailure Unknown(string? msg = null, Exception? innerException = null)
     {
         return new FileFailure(FileFailureType.Unknown, FileFailureMessageKeys.Unknown, msg, innerException);
     } 
